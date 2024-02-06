9ddf6380b302250a672853e076d292bd1c6dc393
VsIakushev <vsiakushev@gmail.com>
2024-02-06 13:18:34 -0800

    Настроил шаблон, с подключенными и настроенными SwiftLint и SwiftFormat, а так же gitignore. Размещаю это в Develop. Все задания буду ответвлять отсюда.

--- UIKitTemplate/UIKitTemplate/ViewController.swift
+++ UIKitTemplate/UIKitTemplate/ViewController.swift
@@ -1,19 +1,11 @@
-//
-//  ViewController.swift
-//  UIKitTemplate
-//
-//  Created by Vitaliy Iakushev on 05.02.2024.
-//
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         print("ViewController")
     }
-
-
 }
-
--- UIKitTemplate/UIKitTemplate/ViewController2ViewController.swift
+++ UIKitTemplate/UIKitTemplate/ViewController2ViewController.swift
@@ -1,17 +1,11 @@
-//
-//  ViewController2ViewController.swift
-//  UIKitTemplate
-//
-//  Created by Vitaliy Iakushev on 05.02.2024.
-//
+// ViewController2ViewController.swift
+// Copyright © RoadMap. All rights reserved.
 
 import UIKit
 
 class ViewController2ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
         print("ViewController2")
     }
 }
